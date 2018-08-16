5991789b45c35a3674ce1072249c17cb947f905b
Rhytah <ritanamono@gmail.com>
2018-08-16 16:56:34 +0300

    add endpoint to add a question

--- api/views.py
+++ api/views.py
@@ -27,7 +27,28 @@ def get_a_question(question_id):
 
 @app.route('/api/v1/questions', methods=['POST'])
 def add_a_question():
-    pass
+    request_data = request.get_json()
+    question_id = len (answers) +1
+    subject = request_data.get('subject')
+    asked_by = request_data.get('asked_by')
+    question_date = request_data.get('question_date')
+
+    if not subject or subject == ' ' or subject == type(int):
+        return jsonify({'message':'Please Indicate what you are asking about'})
+
+    if not asked_by or asked_by == ' ' or asked_by == type(int):
+        return jsonify({'message':'Name of person asking question is required'})
+
+    if not question_date or question_date == ' ' :
+        return jsonify({'message':'When was the question asked?'})
+
+    new_question = Question(question_id,subject,asked_by,question_date)
+    questions.append(new_question)
+
+    return jsonify({'message': f'Awesome{asked_by}! You have posted a question, answers will be coming your way'})
+
+
+
 
 
 
