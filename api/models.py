82c1ba610d0210c5ce99560be4bce178d99fb214
Nicholas Turner <mugtrade@gmail.com>
2018-08-17 12:19:49 +0300

    Working copy

--- api/models.py
+++ api/models.py
@@ -1,42 +1,44 @@
+# These is a list
+questions = []
+answers = []
 
-class Question():
-    def __init__(self, question_id,subject, asked_by,question_date):
+# These is a dict
+q = {}
+a = {}
+
+
+class Question:
+    def __init__(self, question_id, subject, asked_by, question_date):
         self.question_id = question_id
         self.subject = subject
-        self.asked_by =asked_by
+        self.asked_by = asked_by
         self.question_date = question_date
-        
 
-        def get_question_id(self):
-            return self.question_id
+    def get_question_id(self):
+        return self.question_id
 
-        def get_asked_by(self):
-            return self.asked_by
+    def get_asked_by(self):
+        return self.asked_by
 
-        def get_question_date(self):
-            return self.question_date
+    def get_question_date(self):
+        return self.question_date
 
-        def get_subject(self):
-            return self.subject
+    def get_subject(self):
+        return self.subject
 
-questions = []
 
-class Answer():
-    def __init__(self,answer_id, description, answer_date):
+class Answer:
+    def __init__(self, answer_id, description, answer_date):
         self.answer_id = answer_id
-        self.description =description
+        self.description = description
         self.answer_date = answer_date
 
     def get_answer_id(self):
         return self.answer_id
-        
+
     def get_description(self):
         return self.description
 
     def get_answer_date(self):
         return self.answer_date
-
-answers = []
-
-
 
--- api/views.py
+++ api/views.py
@@ -1,20 +1,22 @@
-from flask import Flask, json,request,jsonify
+from flask import Flask, json, request, jsonify
 
 from .models import Question, Answer, questions, answers
 
 app = Flask(__name__)
 
+
 @app.route('/api/v1/questions', methods=['GET'])
 def get_all_questions():
     if len(questions) > 0:
-        return jsonify({'message':questions}) ###this returns questions list
+        return jsonify({'message': questions})  # this returns questions list
     else:
         return jsonify({
             'status': 'Fail',
-            'message':'There are no questions found on the forum'
+            'message': 'There are no questions found on the forum'
         })
-    
-@app.route('/api/v1/questions/<question_id>', methods =['GET'])
+
+
+@app.route('/api/v1/questions/<question_id>', methods=['GET'])
 def get_a_question(question_id):
     for specific_question in questions:
         if specific_question.get(id) == question_id:
@@ -22,37 +24,31 @@ def get_a_question(question_id):
 
     return jsonify({
         'status': 'Fail',
-        'message':'Question doesnot exist'
+        'message': 'Question does not exist'
     })
+
 
 @app.route('/api/v1/questions', methods=['POST'])
 def add_a_question():
     request_data = request.get_json()
-    question_id = len (answers) +1
+    question_id = len(answers) + 1
     subject = request_data.get('subject')
     asked_by = request_data.get('asked_by')
     question_date = request_data.get('question_date')
 
     if not subject or subject == ' ' or subject == type(int):
-        return jsonify({'message':'Please Indicate what you are asking about'})
+        return jsonify({'message': 'Please Indicate what you are asking about'})
 
     if not asked_by or asked_by == ' ' or asked_by == type(int):
-        return jsonify({'message':'Name of person asking question is required'})
+        return jsonify({'message': 'Name of person asking question is required'})
 
-    if not question_date or question_date == ' ' :
-        return jsonify({'message':'When was the question asked?'})
+    if not question_date or question_date == ' ':
+        return jsonify({'message': 'When was the question asked?'})
 
-    new_question = Question(question_id,subject,asked_by,question_date)
+    new_question = Question(question_id, subject, asked_by, question_date)
     questions.append(new_question)
 
-    return jsonify({'message': f'Awesome{asked_by}! You have posted a question, answers will be coming your way'})
-
-
-
-
-
-
-
+    return jsonify({'message': 'Awesome{asked_by}! You have posted a question, answers will be coming your way'})
 
 
 if __name__ == '__main__':
