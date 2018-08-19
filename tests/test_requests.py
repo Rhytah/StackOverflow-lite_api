b399118a2a330b5cbc6779821401b9871e481503
Rhytah <ritanamono@gmail.com>
2018-08-20 01:09:06 +0300

    test add a question

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -29,3 +29,12 @@ class RequestTestmodels(BaseTestCase):
             "Hello Rhytah! Question successfully added", str(response.data)
         )
 
+    def test_add_an_answer(self):
+        
+        response=self.test_client.post(
+            '/api/v1/questions/1/answers', data = json.dumps(self.request_data),content_type='application/json')
+        self.assertEqual(response.status_code,200)
+        self.assertIn(
+            'Great job! answer added to question 1', str(response.data)
+        )
+
