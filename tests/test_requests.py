f147799ad5a9695bdc2812d64440a85eb1887cb8
Rhytah <ritanamono@gmail.com>
2018-08-22 16:22:07 +0300

    add more tests

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -21,6 +21,7 @@ class RequestTestmodels(BaseTestCase):
         )
         self.assertEqual(response.status_code,404)
 
+
     def test_add_a_question(self):
         response=self.test_client.post(
             '/api/v1/questions', data = json.dumps(self.request_data),content_type='application/json')
@@ -28,6 +29,14 @@ class RequestTestmodels(BaseTestCase):
         self.assertIn(
             "Hello Rhytah! Question successfully added", str(response.data)
         )
+    def test_add_a_question_without_subject(self):
+    
+        response = self.test_client.post('/api/v1/questions',
+        data=json.dumps({"question_id": 2 , "subject": " ","asked_by": "Tom", "question_date": "Yesterday"}),
+        content_type='application/json')
+        self.assertEqual(response.status_code,200)
+        self.assertIn('Please Indicate what you are asking about',str(response.data))
+
 
     def test_add_an_answer(self):
         
@@ -37,4 +46,10 @@ class RequestTestmodels(BaseTestCase):
         self.assertIn(
             'Great job! answer added to question 1', str(response.data)
         )
-
+    
+    def test_get_a_question_with_invalid_id(self):
+        response=self.test_client.post('/api/v1/questions')
+        response=self.test_client.get('/api/v1/question/6', data=json.dumps(self.request_data), content_type='application/json')
+        self.assertEqual(response.status_code,404)
+        
+    
